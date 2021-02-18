74dfae1967cd388a5be44b0c48fa383e762f5f32
SVETLANA TODOROVA <svetlana.todorova@student.hogent.be>
2021-02-18 21:50:20 +0100

    connectie met databank

---
+++ src/persistentie/Connectie.java
@@ -0,0 +1,7 @@
+package persistentie;
+
+public class Connectie {
+
+     static final String JDBC_URL = 
+    		"jdbc:mysql://ID344009_g99.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID344009_g99&password=slbjg99!";
+}
