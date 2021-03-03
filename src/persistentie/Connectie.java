9fe3c8e4c195893a4a506b29f7d150caab77effc
JOOST DE CRAEMER <joost.decraemer@student.hogent.be>
2021-03-03 13:14:29 +0100

    Connectie klasse aangemaakt

---
+++ src/persistentie/Connectie.java
@@ -0,0 +1,8 @@
+package persistentie;
+
+public class Connectie {
+    /*
+     * INFO: Hieronder username en het paswoord aangepast volgens gegevens verwijziging naar GR99database.
+     */
+    public static final String JDBC_URL = "jdbc:mysql://ID344009_g99.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID344009_g99&password=slbj99!";
+}
