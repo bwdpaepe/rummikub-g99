16188ab261c012fd50c4fc8c1d9ad92cb3dc63c7
BART DE PAEPE <BART.DEPAEPE@student.hogent.be>
2021-03-16 22:51:48 +0100

    Generate controllers and update module-info.java

---
+++ src/gui/GeefAantalGebruikersController.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+
+public class GeefAantalGebruikersController extends BorderPane {
+	@FXML
+	private Label lblAantalSpelers;
+	@FXML
+	private TextField txfAantalSpelers;
+	@FXML
+	private Button btnAantalSpelers;
+
+}
