58969333b1fec34b847bbe8e2cd4096b3db162ca
BART DE PAEPE <BART.DEPAEPE@student.hogent.be>
2021-04-14 21:55:49 +0200

    regenerating the controllers from the GUI UC2

--- src/gui/SpelSituatieController.java
+++ src/gui/SpelSituatieController.java
@@ -1,27 +1,25 @@
 package gui;
 
-import java.io.IOException;
-import java.net.URL;
-
-import domein.DomeinController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+import domein.DomeinController;
+import javafx.event.ActionEvent;
+
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import talen.Language;
-import java.util.ResourceBundle;
 
-public class SpelSituatieController extends BorderPane{
-    @FXML
-    private Label lblSpelersNaam;
-    @FXML
-    private Button btnEindeBeurt;
-	
+public class SpelSituatieController extends BorderPane {
+	@FXML
+	private Label lblSpelersNaam;
+	@FXML
+	private Button btnEindeBeurt;
+
 	private DomeinController dc;
 
 	public SpelSituatieController(DomeinController dc) {
@@ -40,22 +38,20 @@ public class SpelSituatieController extends BorderPane{
 		}
 	}
 	
-	public void btnSpeelBeurtOnAction(ActionEvent event) {
+	// Event Listener on Button[#btnEindeBeurt].onAction
+	@FXML
+	public void bntEindeBeurtOnAction(ActionEvent event) {
 		// terug naar ToonSpelerAanBeurt !?
 		Scene newScene = new Scene(new ToonSpelerAanBeurtController(this.dc));
 		Stage stage = (Stage) this.getScene().getWindow();
-        stage.setScene(newScene);
-        stage.show();
-		
-        //Exception Tijdelijk werkveld moet leeg zijn
-        
-        
-        /*Toepassen methodes ?
-         * Methode (SteenKrijgenIndienNietsAfgeled)
-         */
-        
+		stage.setScene(newScene);
+		stage.show();
+				
+		//Exception Tijdelijk werkveld moet leeg zijn
+		        
+		        
+		/*Toepassen methodes ?
+		 * Methode (SteenKrijgenIndienNietsAfgeled)
+		 */
 	}
-	
-	
-
 }
